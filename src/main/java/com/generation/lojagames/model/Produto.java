238fd8b1ca1a8cd6d090d892451a8be2cc7ecf22
CarolFAraujo <carolina.fac@gmail.com>
2023-04-27 21:38:39 -0300

    Loja de Games

--- src/main/java/com/generation/lojagames/model/Produto.java
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -46,6 +46,11 @@ public class Produto {
 	@ManyToOne
 	@JsonIgnoreProperties("produto")
 	private Categoria categoria;
+	
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
 	public Long getId() {
 		return id;
@@ -109,6 +114,14 @@ public class Produto {
 
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 
 }
